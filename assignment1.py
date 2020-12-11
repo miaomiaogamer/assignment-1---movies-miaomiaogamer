72774efcb01899dfaccf96d43bad8673bc19bddd
breakfastset <34071068+breakfastset@users.noreply.github.com>
2020-12-11 10:09:03 +0800

    Initial commit

---
+++ assignment1.py
@@ -0,0 +1,15 @@
+"""
+Replace the contents of this module docstring with your own details
+Name:
+Date started:
+GitHub URL:
+"""
+
+
+def main():
+    """..."""
+    print("Movies To Watch 1.0 - by <Your Name>")
+
+
+if __name__ == '__main__':
+    main()
